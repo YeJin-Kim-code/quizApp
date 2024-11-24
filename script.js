13cfaf6f2a5d25ac3ebe38032d61518135df7e41
YeJin-Kim-code <109929675+YeJin-Kim-code@users.noreply.github.com>
2024-11-24 15:15:45 +0900

    feat login btn and alert

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('login-btn').addEventListener('click',()=>
+{
+    alert('김유진 멍청이');
+});
+document.getElementById('signup-btn').addEventListener('click',()=>
+{
+    alert('노을이 사랑해');
+    alert('바보바보바보');
+});
